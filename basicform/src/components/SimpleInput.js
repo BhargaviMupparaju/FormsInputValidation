9f8c4c59ba73d77441f5b4f8c986e97b700ab538
BhargaviMupparaju <105601164+BhargaviMupparaju@users.noreply.github.com>
2023-07-25 14:55:22 -0400

    Added was touched and blur validation

--- basicform/src/components/SimpleInput.js
+++ basicform/src/components/SimpleInput.js
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
 	const nameInputRef = useRef();
 	const [enteredName, setEnteredName] = useState("");
 
-  
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+	// Here we are assuming the enteredname is valid at the intial stage which is not convincing.
+	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+	useEffect(() => {
+		if (enteredNameIsValid) {
+			console.log("Name Input is valid");
+		}
+	}, [enteredNameIsValid]);
 
 	const nameInputChangeHandler = (event) => {
 		setEnteredName(event.target.value);
+		if (enteredName.trim() !== "") {
+			setEnteredNameIsValid(true);
+			return;
+		}
 	};
 
+	const nameInputBlurHandler = (event) => {
+		setEnteredNameTouched(true);
+		if (enteredName.trim() === "") {
+			// As soon as if the user start entering the value the error message should go.
+			setEnteredNameIsValid(false);
+			return;
+		}
+	};
 	const formSubmissionHandler = (event) => {
 		event.preventDefault();
-		console.log(enteredName);
+
+		setEnteredNameTouched(true); // In form submission it is clear that the inputs are touched.
+
+		if (enteredName.trim() === "") {
+			setEnteredNameIsValid(false);
+			return;
+		}
+
+		setEnteredNameIsValid(true);
+
 		const enteredValue = nameInputRef.current.value;
 		console.log(enteredValue);
-		setEnteredName("");
 
-  
+		setEnteredName("");
 	};
+
+	const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+
+	const nameInputClasses = enteredNameIsValid ? "form-control" : "form-control invalid";
 
 	return (
 		<form onSubmit={formSubmissionHandler}>
-			<div className="form-control">
+			<div className={nameInputClasses}>
 				<label htmlFor="name">Your Name</label>
-				<input ref={nameInputRef} type="text" id="name" onChange={nameInputChangeHandler} value={enteredName} />
+				<input ref={nameInputRef} type="text" id="name" onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName} />
+				{!enteredNameIsValid && <p className="error-text">Name is not valid</p>}
 			</div>
 			<div className="form-actions">
 				<button>Submit</button>
